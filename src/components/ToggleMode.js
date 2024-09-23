2b4eba31fc52c5ae7957137566ed3878e4728116
Ved <vedbhai900@gmail.com>
2024-09-23 17:05:20 +0530

    Css added

--- src/components/ToggleMode.js
+++ src/components/ToggleMode.js
@@ -2,9 +2,13 @@ import {useState,useEffect} from 'react'
 
 export default function ToggleMode(){
     const[isToggle,setIsToggle]=useState(false)
+    useEffect(()=>{
+      document.body.style.backgroundColor=isToggle?'black':'white'
+      document.body.style.color=isToggle?'white':'black'
+    },[isToggle])
     return(
         <>
-        <button onClick={()=>setIsToggle(!isToggle)}>{isToggle?'Light Mode':'Dark Mode'}</button>
+        <button onClick={()=>setIsToggle(!isToggle)} className='toggle'>{isToggle?'Light Mode':'Dark Mode'}</button>
         </>
     )
 }

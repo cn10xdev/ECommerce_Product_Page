512ead2abe62d94aa381bc06f821ee1319aa6dac
SANSKAR GUPTA <sanskarguptabaisali@gmail.com>
2024-09-26 22:53:28 +0530

    Complete home footer

---
+++ src/constants/social.js
@@ -0,0 +1,38 @@
+import discordBlack from "../assets/socials/discord.svg";
+import facebook from "../assets/socials/facebook.svg";
+import instagram from "../assets/socials/instagram.svg";
+import telegram from "../assets/socials/telegram.svg";
+import twitter from "../assets/socials/twitter.svg";
+
+export const socials = [
+  {
+    id: "0",
+    title: "Discord",
+    iconUrl: discordBlack,
+    url: "#",
+  },
+  {
+    id: "1",
+    title: "Twitter",
+    iconUrl: twitter,
+    url: "#",
+  },
+  {
+    id: "2",
+    title: "Instagram",
+    iconUrl: instagram,
+    url: "#",
+  },
+  {
+    id: "3",
+    title: "Telegram",
+    iconUrl: telegram,
+    url: "#",
+  },
+  {
+    id: "4",
+    title: "Facebook",
+    iconUrl: facebook,
+    url: "#",
+  },
+];

a1ff97086a11f23e2881ee8795524f7922f307c1
SANSKAR GUPTA <sanskarguptabaisali@gmail.com>
2024-09-27 20:16:41 +0530

    Complete cart page

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,29 +1,88 @@
+/* Importing Necessary files, module etc */
 import styles from "./Cart.module.css";
+import { useEffect, useState } from "react";
+import CartItems from "../../components/CartItems/CartItems";
+import NoItemInCart from "../../components/NoItemsInCart/NoItemsInCart";
 
-const Cart = ({ cart, updateQuantity }) => {
+function Cart({ cart, updateQuantity }) {
+  // Defining useState hook name priceBreakup to store totals
+  const [priceBreakUp, setPriceBreakup] = useState({
+    totalPrice: 0,
+    totalDiscount: 0,
+    totalItem: 0,
+    totalMRP: 0,
+  });
+
+  // Using useEffect hook to calculate totals on mounting and whenever user data changes
+  useEffect(() => {
+    if (cart.length > 0) {
+      let totalPr = 0;
+      let totalMrp = 0;
+      cart.map((cartItem) => {
+        totalPr += cartItem.price * cartItem.quality;
+        totalMrp += cartItem.mrp * cartItem.quality;
+      });
+      setPriceBreakup({
+        totalPrice: totalPr,
+        totalDiscount: totalMrp - totalPr,
+        totalItem: cart.length,
+        totalMRP: totalMrp,
+      });
+    }
+  }, [cart]);
+
+  /* Returning the JSX */
   return (
-    <div className="cart">
-      <h2>Cart</h2>
+    <>
       {cart.length === 0 ? (
-        <p>Your cart is empty</p>
+        <NoItemInCart />
       ) : (
-        cart.map((item) => (
-          <div key={item.id} className="cart-item">
-            <p>{item.name}</p>
-            <p>Price: ${item.price}</p>
-            <input
-              type="number"
-              min="1"
-              value={item.quantity}
-              onChange={(e) =>
-                updateQuantity(item.id, parseInt(e.target.value))
-              }
-            />
+        <div className="w-full flex items-center justify-center shadow-xl h-screen bg-n-8 relative pt-[4rem] text-white mt-[1rem]">
+          <div
+            className={`${styles.cartItemsAndPriceContainer} bg-radial-gradient from-[#28206C] to-[#28206C]/0 to-60% bg-n-8 shadow-2xl`}
+          >
+            <div className={styles.cartItemsAndOrderContainer}>
+              <CartItems cart={cart} />
+            </div>
+            <div className={styles.priceBreakUpContainer}>
+              <div className={styles.priceDetailsHeader}>
+                <p>PRICE DETAILS</p>
+              </div>
+              <div className="divider divider-primary mt-0 mb-0"></div>
+              <div className={styles.priceContainer}>
+                <p>Price ({priceBreakUp.totalItem} Items)</p>
+                <p>&#8377; {priceBreakUp.totalMRP}</p>
+              </div>
+              <div className={styles.priceContainer}>
+                <p>Discount</p>
+                <p className={styles.textGreen}>
+                  - &#8377; {priceBreakUp.totalDiscount}
+                </p>
+              </div>
+              <div className={styles.priceContainer}>
+                <p>Delivery Charges</p>
+                <p>
+                  <span className={styles.delivery}>&#8377;240</span>
+                  <span className={styles.textGreen}> Free</span>
+                </p>
+              </div>
+              <div className={styles.totalAmountContainer}>
+                <h4>Total Amount</h4>
+                <h4>&#8377; {priceBreakUp.totalPrice}</h4>
+              </div>
+              <div className={styles.priceContainer}>
+                <p className={styles.textGreen}>
+                  You will save ₹{priceBreakUp.totalDiscount + 240} on this
+                  order
+                </p>
+              </div>
+            </div>
           </div>
-        ))
+        </div>
       )}
-    </div>
+    </>
   );
-};
+}
 
+/* Exporting the Cart Component */
 export default Cart;

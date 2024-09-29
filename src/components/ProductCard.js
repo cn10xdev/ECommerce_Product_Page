38a74ab945935ac236a48f761257e14514d6e3e4
SOURABHs23 <sourabhgarg873@gmail.com>
2024-09-29 15:00:34 +0530

    k

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React from "react";
 
 function ProductCard({ product, addToCart }) {
-    return (
-        <div className="product-card">
-            <img src={product.image} alt={product.name} className="product-image" />
-            <h2>{product.name}</h2>
-            <p>Price: ${product.price}</p>
-            <button onClick={() => addToCart(product)}>Add to Cart</button>
-        </div>
-    );
+  return (
+    <div className="product-card">
+      <img src={product.image} alt={product.name} className="product-image" />
+      <h2>{product.name}</h2>
+      <p>Price: ${product.price}</p>
+      <p>Available Quantity: {product.quantity}</p>
+      <button
+        onClick={() => addToCart(product)}
+        disabled={product.quantity === 0} // Disable button if quantity is zero
+      >
+        {product.quantity > 0 ? "Add to Cart" : "Out of Stock"}
+      </button>
+    </div>
+  );
 }
 
 export default ProductCard;
